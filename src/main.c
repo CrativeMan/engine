73979b60282b5a9fabe11b7e4d9c9e0d99d2785d
crative <hannig.sebi@gmail.com>
2024-10-21 20:25:02 +0200

    snake clone start

--- src/main.c
+++ src/main.c
@@ -1,64 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-#include <GL/glew.h>
+#include <GL/gl.h>
 #include <GLFW/glfw3.h>
 
-static const GLfloat g_vertex_buffer_data[] = {-1.0f, -1.0f, 0.0f, 1.0f, -1.0f,
-                                               0.0f,  0.0f,  1.0f, 0.0f};
+/* defines */
+
+#define WIN_WIDTH 512
+#define WIN_HEIGHT 512
 
 int main() {
-  glewExperimental = GL_TRUE;
 
   if (!glfwInit()) {
-    fprintf(stderr, "Failed to initialize glfw\n");
-    return -1;
+    fprintf(stderr, "[ERROR]: Failed to init GLFW!\n");
+    exit(EXIT_FAILURE);
   }
-
-  glfwWindowHint(GLFW_SAMPLES, 4);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-  GLFWwindow *window;
-  window = glfwCreateWindow(1024, 768, "engine", NULL, NULL);
+  GLFWwindow *window =
+      glfwCreateWindow(WIN_WIDTH, WIN_HEIGHT, "snake_clone", NULL, NULL);
   if (window == NULL) {
-    fprintf(stderr, "Failed to open GLFW window.");
+    fprintf(stderr, "[ERROR]: Failed creating GLFW window!\n");
     glfwTerminate();
-    return -1;
+    exit(EXIT_FAILURE);
   }
-
-  GLuint vao;
-  glGenVertexArrays(1, &vao);
-  glBindVertexArray(vao);
-
-  GLuint vertexbuffer;
-  glGenBuffers(1, &vertexbuffer);
-  glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data),
-               g_vertex_buffer_data, GL_STATIC_DRAW);
-
   glfwMakeContextCurrent(window);
-  glewExperimental = GL_TRUE;
-  if (glewInit() != GLEW_OK) {
-    fprintf(stderr, "Failed to init GLEW\n");
-    return -1;
-  }
 
-  glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
-  do {
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    glEnableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void *)0);
-
-    glDrawArrays(GL_TRIANGLES, 0, 3);
-    glDisableVertexAttribArray(0);
-
+  while (!glfwWindowShouldClose(window)) {
     glfwSwapBuffers(window);
     glfwPollEvents();
-  } while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
-           glfwWindowShouldClose(window) == 0);
+  }
+
+  glfwTerminate();
+  return 0;
 }
