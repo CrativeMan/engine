854509cb800927487d692abb6e89c53d2fe4db03
crative <hannig.sebi@gmail.com>
2024-10-24 21:41:28 +0200

    colorful logging

--- src/logger.c
+++ src/logger.c
@@ -2,10 +2,15 @@
 
 #include "header/logger.h"
 
+const char *RESET = "\033[0m";
+const char *RED = "\033[0;31m";
+const char *GREEN = "\033[0;32m";
+const char *CYAN = "\033[0;36m";
+
 void loggerInfo(char *id, char *message) {
-  fprintf(stdout, "[Info] [%s] %s\n", id, message);
+  fprintf(stdout, "%s[Info] [%s] %s%s\n", CYAN, id, message, RESET);
 }
 
 void loggerError(char *id, char *message) {
-  fprintf(stderr, "[Error] [%s] %s\n", id, message);
+  fprintf(stderr, "%s[Error] [%s] %s%s\n", RED, id, message, RESET);
 }
