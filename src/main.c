1f28143e0de1c69ae87e46c0d4183913fa3a9d8a
crative <hannig.sebi@gmail.com>
2024-10-24 12:05:53 +0200

    refactor

--- src/main.c
+++ src/main.c
@@ -10,7 +10,6 @@
 #define WIN_HEIGHT 600
 
 float vertices[] = {
-    // first triangle
     0.5f,  0.5f,  0.0f, // top right
     0.5f,  -0.5f, 0.0f, // bottom right
     -0.5f, 0.5f,  0.0f, // top left
