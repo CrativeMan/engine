bebe94fa1ea7cdb4d082f5c2d4cb089c80636524
Sebastian Hannig <hannig.sebi@gmail.com>
2024-11-02 12:49:36 +0100

    logger changes

--- src/logger.c
+++ src/logger.c
@@ -8,7 +8,7 @@
 
 const char *RESET = "\033[0m";
 const char *RED = "\033[0;31m";
-const char *YELLOW = "\003[0;33m";
+const char *YELLOW = "\033[0;33m";
 const char *GREEN = "\033[0;32m";
 const char *CYAN = "\033[0;36m";
 
